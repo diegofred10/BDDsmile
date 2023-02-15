4c301b8958e1a286e5fd24fe012b3d1efa668520
diegofred10 <diegoa.gsm@gmail.com>
2023-02-15 10:29:46 +0100

    fist

---
+++ src/test/java/com/factoriaf5/smile/SmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.smile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
